dab7e5ea4f07ae525ae6622cfdd2608748391874
HaroldVillalba-Raizul <haroldvillalba@raizul.co>
2025-06-26 18:57:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    password = Column(String)
 
 # Crear tablas si no existen
 Base.metadata.create_all(bind=engine)
