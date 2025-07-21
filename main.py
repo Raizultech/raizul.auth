e4c6587c2f634295c82643b8a6d4d502b1678fb4
HaroldVillalba-Raizul <haroldvillalba@raizul.co>
2025-07-21 09:54:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ async def login(user_data: LoginUser):
         except httpx.HTTPStatusError as e:
             raise HTTPException(status_code=e.response.status_code, detail=e.response.json().get("detail", "Credenciales incorrectas"), headers={"WWW-Authenticate": "Bearer"})
         except httpx.RequestError:
-            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=json.dumps(response_body))
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="El servicio de login interno no está disponible.")
 
 @auth_router.get("/me", response_model=UserResponse)
 def read_users_me(current_user: dict = Depends(get_current_user_from_hidden_api)):
